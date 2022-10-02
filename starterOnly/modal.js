a3dd24a911e0604bf74075fa47d805a3d4483d34
Mathis Charretier <charretiermathis@gmail.com>
2022-10-02 21:28:30 +0200

    Fix #2: Check form fields before submit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,3 +25,66 @@ function launchModal() {
 modalClose.addEventListener("click", function(){
   modalbg.style.display = "none";
 })
+
+// onSubmit modal form
+function validate(event) {
+
+  // Cancels page reload when submit
+  event.preventDefault();
+
+  // Form fields
+  const first = document.querySelector("#first") ? document.querySelector("#first").value : "";
+  const last = document.querySelector("#last") ? document.querySelector("#last").value : "";
+  const email = document.querySelector("#email") ? document.querySelector("#email").value : "";
+  const birthdate = document.querySelector("#birthdate") ? document.querySelector("#birthdate").value : "";
+  const quantity = document.querySelector("#quantity") ? document.querySelector("#quantity").value : "";
+  const location = document.querySelector("[name=location]:checked") ? document.querySelector("[name=location]:checked").value : "";
+  const checkbox1 = document.querySelector("#checkbox1:checked") ? true : false;
+  const checkbox2 = document.querySelector("#checkbox2:checked") ? true : false;
+
+  // Init error state
+  var error = false;
+
+  // Check First field
+  if (first.length < 2) {
+    error = true;
+  }
+
+  // Check First field
+  if (last.length < 2) {
+    error = true;
+  }
+
+  // Check Email field
+  var mailformat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(!email.match(mailformat)) {
+    error = true;
+  }
+  
+  // Check Quantity field
+  if (isNaN(quantity)) {
+    error = true;
+  }
+  else if (quantity < 0 || quantity > 99) {
+    error = true;
+  }
+
+  // Check Location field
+  if (location.length <= 0) {
+    error = true;
+  }
+
+  // Check Checkbox1 field
+  if (!checkbox1) {
+    error = true;
+  }
+
+  // If there are no errors
+  if (!error) {
+    modalbg.style.display = "none";
+  }
+  // Else errors are displayed
+  else {
+    console.log("Formulaire invalide !");
+  }
+}
